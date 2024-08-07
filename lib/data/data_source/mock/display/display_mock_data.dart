ea8b10f9dda1d9a84eb40642368f7a388af04243
Dowon Kim <dowoniscool@yahoo.com>
2024-08-07 02:14:11 +0200

    Sprint 1 ECOMFLUT - 25 done : DataLayer - Mock api, dto

---
+++ lib/data/data_source/mock/display/display_mock_data.dart
@@ -0,0 +1,7 @@
+class DisplayMockData {
+  static final String menusByMarket =
+      '[{"title":"Fine Market Recommend"},{"title":"New Arrivals"},{"title":"Best"},{"title":"알뜰쇼핑"},{"title":"특가/혜택"}]';
+
+  static final String menusByBeauty =
+      '[{"title":"Fine Market Recommend"},{"title":"Festival"},{"title":"신상품"},{"title":"베스트"},{"title":"브랜드관"}]';
+}
